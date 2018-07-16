fa996853cc4a1b0472c31c2b86cf3965dff42fba
MirjanaDj <mirjana.jovanovic.sd@gmail.com>
2018-07-16 12:25:24 +0200

    Dodat je folder CSharp

---
+++ Zavrsnitest_CSharp_MirjanaDj/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zavrsnitest_CSharp_MirjanaDj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite vrednost promenljive a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Kada se broj {0} podeli brojem {1}, rezultat je celobrojna vrednost i iznosi {2}.", a, b, rez);
+
+        }
+    }
+}
